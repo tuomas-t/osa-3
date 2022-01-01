4ef8fd64b3330db82ec20a3400c9558c8f7d4461
unknown <tuomas.tiippana@outlook.com>
2022-01-01 20:15:28 +0200

    Ensimmäinen commit

---
+++ index.js
@@ -0,0 +1,94 @@
+const cors = require('cors')
+const express = require('express')
+const morgan = require('morgan')
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+morgan.token('body', (req) => JSON.stringify(req.body))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
+let persons = [
+    {
+      id: 1,
+      name: 'Arto Hellas',
+      number: '040-123456'
+    },
+    {
+        id: 2,
+        name: 'Ada Lovelace',
+        number: '39-44-5323523'
+    },
+    {
+        id: 3,
+        name: 'Dan Abramov',
+        number: '12-43-234345'
+    },
+    {
+        id: 4,
+        name: 'Mary Poppendick',
+        number: '39-23-6423122'
+    }
+]
+
+const generateId = () => {
+    return Math.floor(Math.random() * 1000)
+}
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
+})
+
+app.get('/info', (req, res) => {
+    res.send(`Phonebook has info for ${persons.length} people <br/>${new Date()}`)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(p => p.id === id)
+    if (person) {
+        res.json(person)
+    } else {
+        res.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    persons = persons.filter(p => p.id !== id)
+    res.status(204).end()
+})
+
+app.post('/api/persons/', (req, res) => {
+    if (persons.find(p => p.name === req.body.name)) {
+        return res.status(400).json({ 
+            error: 'name must be unique' 
+        })
+    } else if (!req.body.name) {
+        return res.status(400).json({ 
+            error: 'name missing' 
+        })
+    } else if (!req.body.number) {
+        return res.status(400).json({ 
+            error: 'number missing' 
+        })
+    }
+    const person = {
+        id: generateId(),
+        name: req.body.name,
+        number: req.body.number,
+    }
+    persons = persons.concat(person)
+    res.json(person)
+})
+
+const unknownEndpoint = (req, res) => {
+    res.status(404).send({ error: 'unknown endpoint' })
+}
+
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
