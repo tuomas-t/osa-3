6dd9821abba8b1d97d29c7eff9373281fa7f2191
unknown <tuomas.tiippana@outlook.com>
2022-01-02 16:13:16 +0200

    almost forgot

--- mongo.js
+++ mongo.js
@@ -9,26 +9,26 @@ const url = `mongodb+srv://fstack:${password}@cluster0.pzxny.mongodb.net/test?re
 mongoose.connect(url)
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String,
+  name: String,
+  number: String,
 })
-  
+
 const Person = mongoose.model('Person', personSchema)
 
 if (name && number) {
-    const person = new Person({
-        name: name,
-        number: number
-    })
-    person.save().then(() => {
-        mongoose.connection.close()
-    })
-    console.log(`added ${name} number ${number} to your phonebook`)
+  const person = new Person({
+    name: name,
+    number: number
+  })
+  person.save().then(() => {
+    mongoose.connection.close()
+  })
+  console.log(`added ${name} number ${number} to your phonebook`)
 } else {
-    Person.find({}).then(result => {
-        result.forEach(p => {
-            console.log(`${p.name} ${p.number}`)
-        })
-        mongoose.connection.close()
+  Person.find({}).then(result => {
+    result.forEach(p => {
+      console.log(`${p.name} ${p.number}`)
     })
+    mongoose.connection.close()
+  })
 }
